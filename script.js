6736ec9e6981284102e9c7991e882e68e64ec04a
samrat9x <primeminister9xm@gmail.com>
2025-05-27 19:18:50 +0600

    Add GitHub corner logo and link to repository

--- script.js
+++ script.js
@@ -9,3 +9,9 @@ const today = new Date();
 const yearContainer = document.querySelector(".year-container");
 const currentYear = today.getFullYear();
 yearContainer.textContent = currentYear;
+
+document
+  .querySelector(".github-corner")
+  .addEventListener("click", (e) =>
+    window.open("https://github.com/samrat9x/My_Tiny_Projects", "_blank")
+  );
